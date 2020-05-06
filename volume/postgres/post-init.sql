e97daea7d4f06470e6a0a7defefec26131fde027
eweber_42 <eweber@korben.com>
2020-05-06 13:06:00 +0200

    update stock if new product trigger

--- volume/postgres/post-init.sql
+++ volume/postgres/post-init.sql
@@ -9,6 +9,7 @@ create table stocks as
 
 alter table stocks add foreign key (ean) references products(ean);
 
+--Trigger d'update des stock si on update le state d'un item
 
 create or replace function update_stock_item_state() returns trigger as $update_stock_item_state$
     declare
@@ -32,6 +33,31 @@ $update_stock_item_state$ language plpgsql;
 
 DROP TRIGGER IF EXISTS update_stock_item_state on items;
 
-create trigger update_stock_item_state after update on items
+create trigger update_stock_item_state after update or insert on items
     for each row
     execute procedure update_stock_item_state();
+
+--Trigger d'update des stocks si on crée un item
+
+create or replace function insert_stock_new_product() returns trigger as $insert_stock_new_product$
+    declare
+        this_new_ean products.ean%TYPE;
+    begin
+        select into this_new_ean new.ean;
+
+        insert into stocks (ean, stock)
+        select  this_new_ean as ean,
+                (select count(*)
+                from items
+                where items.state in ('STOCK', 'SALES AREA')
+                and ean = this_new_ean
+                ) as ean;
+        return null;
+    end;
+$insert_stock_new_product$ language plpgsql;
+
+DROP TRIGGER IF EXISTS insert_stock_new_product on products;
+
+create trigger insert_stock_new_product after insert on products
+    for each row
+    execute procedure insert_stock_new_product();
